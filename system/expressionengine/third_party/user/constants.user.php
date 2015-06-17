48e64e90027dcb7b41b1e37087d053980785f9ff
Ivandevp <medinadiazivan@gmail.com>
2015-06-17 11:25:52 -0500

    User module installed

---
+++ system/expressionengine/third_party/user/constants.user.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * User - Constants
+ *
+ * @package		Solspace:User
+ * @author		Solspace, Inc.
+ * @copyright	Copyright (c) 2008-2015, Solspace, Inc.
+ * @link		http://solspace.com/docs/user
+ * @license		http://www.solspace.com/license_agreement
+ * @version		3.5.3
+ * @filesource	user/constants.user.php
+ */
+
+if ( ! defined('USER_VERSION'))
+{
+	define('USER_VERSION',	'3.5.3');
+	define('USER_DOCS_URL',	'http://solspace.com/docs/user/');
+}
