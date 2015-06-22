33ed6dcc173056a5bdb23d406830fec41894e475
MaritzaVst <maritzasintitafur@gmail.com>
2015-06-22 18:55:07 -0500

    Últimos cambios

--- frontend/app/assets/js/scroll-control.js
+++ frontend/app/assets/js/scroll-control.js
@@ -1,14 +1,19 @@
 $(document).ready(function(){
-	$(".comment").click(function(event){
+	// $(".comment").click(function(event){
+	// 	event.preventDefault();
+	// 	$(".scroll-down").toggleClass("hidden");
+	// });
+	// $(".responder").click(function(event){
+	// 	event.preventDefault();
+	// 	$(".respuesta").toggleClass("hidden");
+	// });
+	// $(".active-slide").click(function(event){
+	// 	event.preventDefault();
+	// 	$(".list-slide").toggleClass("active");
+	// });
+	$("#up").click(function(event){
 		event.preventDefault();
-		$(".scroll-down").toggleClass("hidden");
-	});
-	$(".responder").click(function(event){
-		event.preventDefault();
-		$(".respuesta").toggleClass("hidden");
-	});
-	$(".active-slide").click(function(event){
-		event.preventDefault();
-		$(".list-slide").toggleClass("active");
+		$("footer").toggleClass("active-up");
 	});
 });
+
