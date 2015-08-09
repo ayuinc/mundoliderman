e9c778c842bae07f3ea3bf7045db3295c7368bdc
Iván Medina Díaz <medinadiazivan@gmail.com>
2015-08-09 12:20:33 -0500

    Mensaje de capacitaciones y títulos

--- system/expressionengine/third_party/webservice/pi.webservice.php
+++ system/expressionengine/third_party/webservice/pi.webservice.php
@@ -258,7 +258,7 @@ Plugin for retreiving data from Mundo Liderman's Web Service
 		if (count($data) > 0) {
 			return $this->EE->TMPL->parse_variables($this->EE->TMPL->tagdata, $data);
 		} else {
-			return "No tienes préstamos pendientes.";
+			return "<div class='pr-42 pl-21 mh-42 mh-0-xs pt-42 mt-14'>No tienes préstamos pendientes.</div>";
 		}
 	}
 
