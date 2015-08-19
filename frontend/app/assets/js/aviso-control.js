c5b5241016d192f68adc9adf978a416d634da0d3
MaritzaVst <maritzasintitafur@gmail.com>
2015-08-19 17:32:27 -0500

    Bolate de pago lista

--- frontend/app/assets/js/aviso-control.js
+++ frontend/app/assets/js/aviso-control.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	/*RESUELTO Y NO RESUELTO*/
 	$(".aviso").click(function(){
 		$(this).parent().find(".btn-aviso1").toggleClass("hidden");
 		$(this).parent().find(".btn-aviso2").toggleClass("hidden");
@@ -8,4 +9,14 @@ $(document).ready(function(){
 		$(this).parent().find(".like").toggleClass("active-like");
 		$(this).parent().find(".img-like").toggleClass("hidden");
 	});
+
+	/*BTN ELIMINAR PUBLICACIÓN*/
+	$(".eraser").hover(function(){
+		$(".activo").toggleClass("desaparecer");
+		$(".inactivo").toggleClass("desaparecer");
+	});
+	// $(".eraser").mouseleave(function(){
+	// 	$(".activo").removeClass("desaparecer");
+	// 	$(".inactivo").addClass("desaparecer");
+	// });
 });
