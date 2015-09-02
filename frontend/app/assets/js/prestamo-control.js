28b643d0e2a251ee458491076040d8dbcef1497d
MaritzaVst <maritzasintitafur@gmail.com>
2015-09-02 14:24:00 -0500

    Solicitud de préstamo responsive

--- frontend/app/assets/js/prestamo-control.js
+++ frontend/app/assets/js/prestamo-control.js
@@ -4,8 +4,10 @@ $(document).ready(function(){
 		var secondLine = $(".line").slice(1,2); /*Permite seleccionar le 2do elemento*/
 		if(value == "Otro") {
 			$(secondLine).attr("style", "display:block!important");
+			$("#freeform_introduce_el_monto_solicitado").attr("required","required");
 		}else {
 			$(secondLine).removeAttr("style", "display:block!important");
+			$("#freeform_introduce_el_monto_solicitado").removeAttr("required","required");
 		}
 	});
 
