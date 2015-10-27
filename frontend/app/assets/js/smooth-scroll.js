08ce15d2005c528c7619b519d5d12eacf669b7f6
Ivan Medina <medinadiazivan@gmail.com>
2015-10-27 12:40:28 -0500

    Calendario de tareo completo

--- frontend/app/assets/js/smooth-scroll.js
+++ frontend/app/assets/js/smooth-scroll.js
@@ -7,44 +7,42 @@ $(document).on('click', 'a.smooth', function(e) {
     }, 1000);
 });
 
-/*Banner alerta de publicaciones*/
-var positionAlert;
-$(document).ready(function(){
-	positionAlert = $("#alert-publication").offset().top - 82;	
-  var scroll = $(this).scrollTop();
-  console.log("Scroll: "+scroll);
-  console.log("positionAlert: "+positionAlert);
-  showAlert(scroll, positionAlert);
+if ($("#alert-publication").length > 0) {
+  /*Banner alerta de publicaciones*/
+  var positionAlert;
+  $(document).ready(function(){
+  	positionAlert = $("#alert-publication").offset().top - 82;	
+    var scroll = $(this).scrollTop();
+    showAlert(scroll, positionAlert);
 
-  $(".close-alert").on("click", function(){
-    $("#alert-publication").fadeOut();  
+    $(".close-alert").on("click", function(){
+      $("#alert-publication").fadeOut();  
+    });
+
+    $('a.scroll').click(function(e){
+      e.preventDefault();
+      $("#alert-publication").dequeue();
+      $('html, body').stop().animate({scrollTop: $($(this).attr('href')).offset().top}, 1000);
+      var new_posts = $("#new_post").html();
+      $("#post").prepend(new_posts);
+      $("#new_post").html("");
+    }); 
   });
 
-  $('a.scroll').click(function(e){
-    e.preventDefault();
-    $("#alert-publication").dequeue();
-    $('html, body').stop().animate({scrollTop: $($(this).attr('href')).offset().top}, 1000);
-    var new_posts = $("#new_post").html();
-    $("#post").prepend(new_posts);
-    $("#new_post").html("");
-  }); 
-});
+  $(document).scroll(function() {    
+  	var scroll = $(this).scrollTop();
+    showAlert(scroll, positionAlert);
+  });
 
-$(document).scroll(function() {    
-	var scroll = $(this).scrollTop();
-	console.log("Scroll: "+scroll);
-	console.log("positionAlert: "+positionAlert);
-  showAlert(scroll, positionAlert);
-});
-
-function showAlert(scroll, alertPosition) {
-  if (scroll >= alertPosition) {
-      $("#alert-publication").addClass("fixed");
-      $("#alert-publication").addClass("top-banner");
-      $("#alert-publication").removeClass("relative");
-  } else {
-      $("#alert-publication").removeClass("fixed");
-      $("#alert-publication").removeClass("top-banner");
-      $("#alert-publication").addClass("relative");
+  function showAlert(scroll, alertPosition) {
+    if (scroll >= alertPosition) {
+        $("#alert-publication").addClass("fixed");
+        $("#alert-publication").addClass("top-banner");
+        $("#alert-publication").removeClass("relative");
+    } else {
+        $("#alert-publication").removeClass("fixed");
+        $("#alert-publication").removeClass("top-banner");
+        $("#alert-publication").addClass("relative");
+    }
   }
 }
