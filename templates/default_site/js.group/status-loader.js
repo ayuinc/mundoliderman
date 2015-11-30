36f76c44609739b6e81df2b3f0c376f5f53aac26
Ivan Medina <medinadiazivan@gmail.com>
2015-11-30 15:44:16 -0500

    Cargar publicaciones con animación

--- templates/default_site/js.group/status-loader.js
+++ templates/default_site/js.group/status-loader.js
@@ -5,14 +5,19 @@ $(function() {
             $.ajax({
                 url: '{site_url}wall/status/' + offset,
                 method: 'get',
+                beforeSend: function() {
+                    $(".loading-status").fadeIn();
+                },
                 success: function(response) {
                     if (response.length > 0) {
                         $("#post").append(response);
                         offset += 1;
-	            }
+	                }
+                    $(".loading-status").fadeOut();
                 },
                 error: function(error) {
                     console.log(error);
+                    $(".loading-status").fadeOut();
                 }
             });
         }
