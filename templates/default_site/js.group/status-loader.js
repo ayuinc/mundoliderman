35c03c3e631b896dfda3ced89a5fe82edfcdac4a
Ivan Medina <medinadiazivan@gmail.com>
2015-12-10 10:12:53 -0500

    Validación de offset de publicaciones

--- templates/default_site/js.group/status-loader.js
+++ templates/default_site/js.group/status-loader.js
@@ -1,6 +1,6 @@
 $(function() {
     var offset = 1;
-    var processing = false;
+    var old_offset = 0;
     $(window).scroll(function() {
         var url = '{site_url}wall/status/' + offset;
         var member_id = $("#status_member_id").val();
@@ -13,15 +13,15 @@ $(function() {
                     $(".loading-status").fadeIn();
                 },
                 success: function(response) {
-                    if (!processing) {
-                        processing = true;
+                    var offset_requested = parseInt(this.url.substr(this.url.length - 1, 1));
+                    if (old_offset != offset_requested) {
                         if (response.length > 0) {
                             $("#post").append(response);
+                            old_offset = offset;
                             offset += 1;
                         }
-                        $(".loading-status").fadeOut();
-                        processing = false;
                     }
+                    $(".loading-status").fadeOut();
                 },
                 error: function(error) {
                     console.log(error);
