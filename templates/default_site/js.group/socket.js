bf77ff148cc9e14e8303b42f1331189a6728364b
Hugo Angeles Chavez <Hugo Angeles Chavez>
2016-01-15 16:38:41 -0500

    notificacion permamente

--- templates/default_site/js.group/socket.js
+++ templates/default_site/js.group/socket.js
@@ -33,8 +33,10 @@ $(function() {
             })
             .done(function(post) {
                 $("#new_post").prepend(post);
-                $("#new_post_count").text($("#new_post").find(".post-1").length);
-                $("#alert-publication").fadeIn().delay(5000).fadeOut();
+                var count = $("#new_post").find(".post-1").length;
+                $("#new_post_count").text(count);
+                addNotificationsHeader(count);
+                $("#alert-publication").fadeIn().delay(5000);
             });
             break;
         case 'comment':
