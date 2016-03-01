44bf395957d16f25ebe48615e7812eb9ab96f843
Hugo Angeles Chavez <Hugo Angeles Chavez>
2016-03-01 18:47:32 -0500

    Arreglando paginación de publicaciones

--- templates/default_site/js.group/status-loader.js
+++ templates/default_site/js.group/status-loader.js
@@ -1,11 +1,16 @@
 $(function() {
     var offset = 1;
     var old_offset = 0;
+    var $post = $("#post");
+    var $new_post = $("#new_post");
+    var lock = false;
     $(window).scroll(function() {
-        var url = '{site_url}wall/status/' + offset;
-        var member_id = $("#status_member_id").val();
-        if (member_id) { url += '/' + member_id; }
-        if ($(document).height() - ($(window).scrollTop() + $(window).height()) <= 5 ) {
+        if (!lock && $(document).height() - ($(window).scrollTop() + $(window).height()) <= 5 ) {
+            lock = true;
+            var offset = $post.children('.post').length + $new_post.children('.post').length
+            var url = '{site_url}wall/status/' + offset;
+            var member_id = $("#status_member_id").val();
+            if (member_id) { url += '/' + member_id; }
             $.ajax({
                 url: url,
                 method: 'get',
@@ -13,21 +18,21 @@ $(function() {
                     $(".loading-status").fadeIn();
                 },
                 success: function(response) {
-                    var offset_requested = parseInt(this.url.substr(this.url.length - 1, 1));
-                    if (old_offset != offset_requested) {
-                        if (response.length > 0) {
-                            $("#post").append(response);
-                            old_offset = offset;
-                            offset += 1;
-                        }
+                    
+                    if (response.length > 0) {
+                        $("#post").append(response);
+                        old_offset = offset;
                     }
+
                     formatLinks();
                     $(".loading-status").fadeOut();
+                    lock = false;
                 },
                 error: function(error) {
                     console.log(error);
                     $(".loading-status").fadeOut();
                     processing = false;
+                    lock = false;
                 }
             });
         }
