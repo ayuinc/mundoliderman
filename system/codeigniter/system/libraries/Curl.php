86e13e0af593b598adedf483e6c98f85e132b19f
Claudia Marquez <claudia92@gmail.com>
2017-07-16 23:32:31 -0500

    set timeout por curl requests

--- system/codeigniter/system/libraries/Curl.php
+++ system/codeigniter/system/libraries/Curl.php
@@ -8,6 +8,7 @@ class Curl {
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_URL,$url);
+		curl_setopt($ch, CURLOPT_TIMEOUT,20);
 		$content=curl_exec($ch);
 		$json = json_decode($content, true);
 		$data = $json["Resultado"];
