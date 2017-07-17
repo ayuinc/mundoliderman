9e303538b4b33a7f5a6e6f6f37d1f615f2f3e124
Claudia Marquez <claudia92@gmail.com>
2017-07-16 23:41:53 -0500

    timeout 5 segundos

--- system/codeigniter/system/libraries/Curl.php
+++ system/codeigniter/system/libraries/Curl.php
@@ -8,7 +8,7 @@ class Curl {
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_URL,$url);
-		curl_setopt($ch, CURLOPT_TIMEOUT,20);
+		curl_setopt($ch, CURLOPT_TIMEOUT,5);
 		$content=curl_exec($ch);
 		$json = json_decode($content, true);
 		$data = $json["Resultado"];
